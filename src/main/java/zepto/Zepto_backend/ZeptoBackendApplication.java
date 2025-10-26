7fbc832d62620061c28d17891909e3bd369c3d21
Rahul <rahulmaurya7652@gmail.com>
2025-10-26 23:28:03 +0530

    project setup

---
+++ src/main/java/zepto/Zepto_backend/ZeptoBackendApplication.java
@@ -0,0 +1,13 @@
+package zepto.Zepto_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeptoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeptoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zepto/Zepto_backend/ZeptoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package zepto.Zepto_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeptoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
